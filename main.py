1da314030e7f6936687e5cf7e483ca63ce85b030
bryamlazo166 <bryamlazo166@gmail.com>
2025-12-10 10:12:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import streamlit as st
 from modules import gestion_activos, almacen, monitoreo
 
 st.set_page_config(page_title="CMMS Rendering", layout="wide", page_icon="🏭")
-
-st.sidebar.image("https://cdn-icons-png.flaticon.com/512/2620/2620630.png", width=80)
 st.sidebar.title("CMMS Rendering")
 
-# Menú simplificado y poderoso
 menu = ["Gestión de Activos (Arbol)", "Almacén de Repuestos", "Monitoreo Predictivo"]
 opcion = st.sidebar.radio("Navegación:", menu)
 
 if opcion == "Gestión de Activos (Arbol)":
-    # Este módulo ahora contiene Equipos, Sistemas y Componentes juntos
     gestion_activos.render_gestion_activos()
 
 elif opcion == "Almacén de Repuestos":
