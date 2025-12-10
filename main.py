8f60a140f00c1e4301bb8fb1a210aee1c6feb51a
bryamlazo166 <bryamlazo166@gmail.com>
2025-12-10 08:45:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import streamlit as st
-# Importamos los módulos que acabamos de crear
-from modules import equipos, componentes
+# Agregamos la importación de almacen
+from modules import equipos, componentes, almacen
 
 st.set_page_config(page_title="CMMS Rendering", layout="wide", page_icon="🏭")
-
 st.title("🏭 CMMS Planta Rendering")
-st.sidebar.title("Menú Principal")
 
-# El menú decide qué módulo cargar
-opcion = st.sidebar.radio("Ir a:", ["Maestro de Equipos", "Componentes & Specs"])
+# Menú actualizado
+menu = ["Maestro de Equipos", "Componentes & Specs", "Almacén de Repuestos"]
+opcion = st.sidebar.radio("Ir a:", menu)
 
 if opcion == "Maestro de Equipos":
-    # Llamamos a la FUNCIÓN, no escribimos todo el código aquí
     equipos.render_equipos_view()
 
 elif opcion == "Componentes & Specs":
     componentes.render_componentes_view()
 
-# Aquí agregaremos en el futuro:
-# elif opcion == "Almacén":
-#     almacen.render_almacen_view()
+elif opcion == "Almacén de Repuestos":
+    almacen.render_almacen_view()
