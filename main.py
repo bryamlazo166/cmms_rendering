c48701771629ad1207d8170cde67ce373d03ab60
bryamlazo166 <bryamlazo166@gmail.com>
2025-12-10 22:27:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import streamlit as st
 from modules import gestion_activos, almacen, monitoreo, configurador
 
-st.set_page_config(page_title="CMMS Rendering", layout="wide", page_icon="🏭")
-st.sidebar.image("https://cdn-icons-png.flaticon.com/512/900/900782.png", width=50)
+st.set_page_config(page_title="CMMS SAP-Style", layout="wide", page_icon="🏭")
 st.sidebar.title("CMMS Rendering")
 
-# Menú
-menu = ["Gestión de Activos (Arbol)", "Almacén de Repuestos", "Monitoreo Predictivo", "⚙️ Configuración Familias"]
+menu = ["Gestión de Activos", "Maestro de Clases", "Almacén", "Monitoreo"]
 opcion = st.sidebar.radio("Ir a:", menu)
 
-if opcion == "Gestión de Activos (Arbol)":
+if opcion == "Gestión de Activos":
     gestion_activos.render_gestion_activos()
-
-elif opcion == "Almacén de Repuestos":
-    almacen.render_almacen_view()
-
-elif opcion == "Monitoreo Predictivo":
-    monitoreo.render_monitoreo_view()
-
-elif opcion == "⚙️ Configuración Familias":
+elif opcion == "Maestro de Clases":
     configurador.render_configurador()
+elif opcion == "Almacén":
+    almacen.render_almacen_view()
+elif opcion == "Monitoreo":
+    monitoreo.render_monitoreo_view()
